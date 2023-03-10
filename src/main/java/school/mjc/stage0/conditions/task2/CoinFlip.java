43f9160437945635f896b87f18fb2337b004a62a
zarina <zarinakamalova117@gmail.com>
2023-03-10 16:20:33 +0600

    done

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -1,7 +1,25 @@
 package school.mjc.stage0.conditions.task2;
 
+import java.util.concurrent.Callable;
+
 public class CoinFlip {
     public void throwCoin(int from1UpTo1000) {
+        if(from1UpTo1000<500){
+            System.out.println("Eagle");
+        }
+        else {
+            System.out.println("Tail");
+        }
 
     }
-}
+
+    public static void main(String[] args) {
+
+CoinFlip coin=new CoinFlip();
+coin.throwCoin(700);
+coin.throwCoin(3);
+coin.throwCoin(500);
+    };
+
+    }
+
--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,21 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+
+        if(first>second){
+            System.out.println(first);
+        }
+        else {
+            System.out.println(second);
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+GreatestNumberPrinter printer=new GreatestNumberPrinter();
+printer.printGreatest(12,-400);
+printer.printGreatest(0,7);
+
     }
 }
